a631650d96a8108825a9f3a74af7513b557f6fa8
kingyo_bowls <info@kingyo-bowl.com>
2023-09-29 03:02:05 +0900

    edit file

--- script/scr.js
+++ script/scr.js
@@ -1,10 +1,26 @@
-let splide = new Splide( '#image-carousel', {
-  type   : 'loop',
-  padding: '10%',
-} );
-
-splide.mount();
-
+// document.addEventListener( 'DOMContentLoaded', function () {
+//   let splide = new Splide( '#image-carousel', {
+//     type   : 'loop',
+//     padding: '10%',
+//     breakpoints: {
+//       820: {
+//         padding: '0',
+//       },
+//     }
+//   } );
+//   splide.mount();
+//   let insta = new Splide( '#insta-carousel', {
+//     perPage: 3,
+//     rewind : true,
+//     gap : '1rem',
+//     breakpoints: {
+//       640: {
+//         perPage: 1,
+//       },
+//     }
+//   } );
+//   insta.mount();
+// } );
 
 // protect
 // document.addEventListener("contextmenu", function (e) {
@@ -13,3 +29,10 @@ splide.mount();
 // document.addEventListener("dragstart", function (e) {
 //       e.preventDefault();
 // });
+
+const hamburgerMenu = document.querySelector('.nav-toggle');
+const header = document.querySelector('.body');
+
+hamburgerMenu.addEventListener('click', () => {
+  header.classList.toggle('js-open');
+});
