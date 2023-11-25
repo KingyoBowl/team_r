430c29742efcae2c676cd4783fc01561318bd580
kingyo_bowls <info@kingyo-bowl.com>
2023-11-26 02:58:53 +0900

    add file

--- script/scr.js
+++ script/scr.js
@@ -1,25 +1,25 @@
 document.addEventListener( 'DOMContentLoaded', function () {
-  let splide = new Splide( '#image-carousel', {
-    type   : 'loop',
-    padding: '10%',
-    breakpoints: {
-      820: {
-        padding: '0',
-      },
-    }
-  } );
-  splide.mount();
-  let insta = new Splide( '#insta-carousel', {
-    perPage: 3,
-    rewind : true,
-    gap : '1rem',
-    breakpoints: {
-      640: {
-        perPage: 1,
-      },
-    }
-  } );
-  insta.mount();
+    var topBtn = $('#page-top');
+    topBtn.hide();
+    $(document).ready(function() {
+      var windowWidth = $(window).width();
+      if (windowWidth > 820) {
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 1200) {
+          topBtn.fadeIn();
+      } else {
+          topBtn.fadeOut();
+      }
+  });
+  topBtn.click(function () {
+      $('body,html').animate({
+          scrollTop: 0
+      }, 200);
+      return false;
+  });
+      }
+  });
+
 } );
 
 // protect
@@ -37,17 +37,14 @@ hamburgerMenu.addEventListener('click', () => {
   header.classList.toggle('js-open');
 });
 
-let button = document.querySelector('.scroll');
-let scrollPx = 1300;
-window.onscroll = function() {scrollFunction()};
-function scrollFunction() {
-  if (document.body.scrollTop > scrollPx || document.documentElement.scrollTop > scrollPx) {
-    button.style.display = "block";
-      button.style.opacity = 1;
-  } else {
-    button.style.opacity = 0;
-    setTimeout(function() {
-      button.style.display = "none";
-    }, 500);
-  }
-}
+// let button = document.querySelector('.scroll');
+// let scrollPx = 1300;
+// window.onscroll = function() {scrollFunction()};
+// function scrollFunction() {
+//   if (document.body.scrollTop > scrollPx || document.documentElement.scrollTop > scrollPx) {
+//     button.style.display = "block";
+//     button.style.opacity = 1;
+//   } else {
+//     fadeOut(button,500)
+//   }
+// }
